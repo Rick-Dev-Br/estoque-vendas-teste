a863804040634e980311186bab12890b1d0c2236
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2025-12-12 16:46:47 -0300

    fix deploy

--- database/migrations/2025_12_11_230304_fix_quantidade_column_on_venda_itens_table.php
+++ database/migrations/2025_12_11_230304_fix_quantidade_column_on_venda_itens_table.php
@@ -8,15 +8,32 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('venda_itens', function (Blueprint $table) {
-            $table->renameColumn('quatidade', 'quantidade');
-        });
+
+        if (Schema::hasColumn('venda_itens', 'quantidade')) {
+            // Se já existe, não faça nada
+            return;
+        }
+
+
+        if (Schema::hasColumn('venda_itens', 'quatidade')) {
+            Schema::table('venda_itens', function (Blueprint $table) {
+                $table->renameColumn('quatidade', 'quantidade');
+            });
+        } else {
+
+            Schema::table('venda_itens', function (Blueprint $table) {
+                $table->integer('quantidade')->after('produto_id');
+            });
+        }
     }
 
     public function down(): void
     {
-        Schema::table('venda_itens', function (Blueprint $table) {
-            $table->renameColumn('quantidade', 'quatidade');
-        });
+
+        if (Schema::hasColumn('venda_itens', 'quantidade')) {
+            Schema::table('venda_itens', function (Blueprint $table) {
+                $table->renameColumn('quantidade', 'quatidade');
+            });
+        }
     }
 };
