d0ce345e91e9fc09935fd11dda2cc3aa91b79d56
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2026-01-12 21:31:48 -0300

    fix deploy 15

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('rick.png') }}" type="image/png">
     <title>{{ config('app.name', 'Rick Teste') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
