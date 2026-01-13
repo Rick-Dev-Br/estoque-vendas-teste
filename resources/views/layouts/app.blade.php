b00d98f8484cc7b547874a0eacc647475e31a7f9
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2026-01-12 21:22:35 -0300

    fix deploy 12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Rick Teste') }}</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
