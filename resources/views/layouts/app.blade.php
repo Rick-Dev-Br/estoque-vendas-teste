301ffbeb67ffad2d9b18c74f055159ffdcb528f2
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2026-01-12 21:29:12 -0300

    fix deploy 14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>{{ config('app.name', 'Rick Teste') }}</title>
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @vite(['resources/css/app.css'])
