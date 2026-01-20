5fec28f936d008da96126aaa06ddf7e7d8c7d131
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2026-01-19 21:54:38 -0300

    add new branch

--- config/database.php
+++ config/database.php
@@ -32,14 +32,14 @@ return [
             'driver' => 'mysql',
 
 
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST', '127.0.0.1'),
 
 
-            'port' => env('DB_PORT'),
+            'port' => env('DB_PORT', '3306'),
 
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'database' => env('DB_DATABASE', 'estoque_vendas'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
 
 
             'unix_socket' => '',
